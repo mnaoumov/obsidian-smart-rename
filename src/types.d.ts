de8b6c25fa59917066119864489cb6e03566043a
Michael Naumov <mnaoumov@gmail.com>
2024-01-17 04:10:10 -0600

    Remove typings as it is already in @types/obsidian-typings

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { TFile } from "obsidian";
-
-declare module "obsidian" {
-  interface MetadataCache {
-    getBacklinksForFile: (file: TFile) => {
-      data: Record<string, LinkCache[]>;
-    };
-  }
-}
