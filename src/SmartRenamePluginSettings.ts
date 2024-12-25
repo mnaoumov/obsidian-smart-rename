108830c3cbb04bb210694074863e4be98c861550
Michael Naumov <mnaoumov@gmail.com>
2024-12-25 17:49:17 -0600

    Update libs

--- src/SmartRenamePluginSettings.ts
+++ src/SmartRenamePluginSettings.ts
@@ -1,9 +1,16 @@
+import { PluginSettingsBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginSettingsBase';
+
 import { InvalidCharacterAction } from './InvalidCharacterAction.ts';
 
-export class SmartRenamePluginSettings {
+export class SmartRenamePluginSettings extends PluginSettingsBase {
   public invalidCharacterAction = InvalidCharacterAction.Error;
+
   public replacementCharacter = '_';
   public shouldStoreInvalidTitle = true;
   public shouldUpdateFirstHeader = false;
   public shouldUpdateTitleKey = false;
+  public constructor(data: unknown) {
+    super();
+    this.init(data);
+  }
 }
