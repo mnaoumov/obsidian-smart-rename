090f0af3c62004bab3c26d388f471d7c540f2d4d
Michael Naumov <mnaoumov@gmail.com>
2025-10-01 23:18:20 -0600

    fix: build

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { Plugin } from './Plugin.ts';
 
-// eslint-disable-next-line import-x/no-default-export
+// eslint-disable-next-line import-x/no-default-export -- Obsidian infrastructure requires a default export.
 export default Plugin;
