08bcea057c763774c732ed8635de9c4c29d688b0
Michael Naumov <mnaoumov@gmail.com>
2026-03-04 19:08:16 -0600

    feat: add setting groups

--- src/PluginSettingsTab.ts
+++ src/PluginSettingsTab.ts
@@ -1,6 +1,6 @@
 import { appendCodeBlock } from 'obsidian-dev-utils/HTMLElement';
 import { PluginSettingsTabBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginSettingsTabBase';
-import { SettingEx } from 'obsidian-dev-utils/obsidian/SettingEx';
+import { SettingGroupEx } from 'obsidian-dev-utils/obsidian/SettingGroupEx';
 
 import type { PluginTypes } from './PluginTypes.ts';
 
@@ -11,80 +11,99 @@ export class PluginSettingsTab extends PluginSettingsTabBase<PluginTypes> {
     super.display();
     this.containerEl.empty();
 
-    new SettingEx(this.containerEl)
-      .setName('Invalid characters action')
-      .setDesc('How to process invalid characters in the new title')
-      .addDropdown((dropdown) => {
-        dropdown.addOptions({
-          Error: 'Show error',
-          Remove: 'Remove invalid characters',
-          Replace: 'Replace invalid character with'
-        });
-        this.bind(dropdown, 'invalidCharacterAction', {
-          onChanged: () => {
-            this.display();
-          }
-        });
+    new SettingGroupEx(this.containerEl)
+      .setHeading('Invalid characters')
+      .addSettingEx((setting) => {
+        setting
+          .setName('Invalid characters action')
+          .setDesc('How to process invalid characters in the new title.')
+          .addDropdown((dropdown) => {
+            dropdown.addOptions({
+              Error: 'Show error',
+              Remove: 'Remove invalid characters',
+              Replace: 'Replace invalid characters'
+            });
+            this.bind(dropdown, 'invalidCharacterAction', {
+              onChanged: () => {
+                this.display();
+              }
+            });
+          });
+      })
+      .addSettingEx((setting) => {
+        setting
+          .setName('Replacement character')
+          .setDesc(createFragment((f) => {
+            f.appendText('Character to replace invalid character with.');
+            f.createEl('br');
+            f.appendText('Applicable only if ');
+            appendCodeBlock(f, 'Invalid characters action');
+            f.appendText(' setting is set to ');
+            appendCodeBlock(f, 'Replace invalid characters');
+            f.appendText('.');
+          }))
+          .setDisabled(this.plugin.settings.invalidCharacterAction !== InvalidCharacterAction.Replace)
+          .addText((text) => {
+            text.inputEl.maxLength = 1;
+            this.bind(text, 'replacementCharacter');
+          });
       });
 
-    if (this.plugin.settings.invalidCharacterAction === InvalidCharacterAction.Replace) {
-      new SettingEx(this.containerEl)
-        .setName('Replacement character')
-        .setDesc('Character to replace invalid character with')
-        .addText((text) => {
-          text.inputEl.maxLength = 1;
-
-          this.bind(text, 'replacementCharacter');
-        });
-    }
-
-    if (this.plugin.settings.invalidCharacterAction !== InvalidCharacterAction.Error) {
-      new SettingEx(this.containerEl)
-        .setName('Should store invalid title')
-        .setDesc(createFragment((f) => {
-          f.appendText('Whether to store the title with invalid characters.');
-          f.createEl('br');
-          f.appendText('If disabled, stores the sanitized version');
-        }))
-        .addToggle((toggle) => {
-          this.bind(toggle, 'shouldStoreInvalidTitle');
-        });
-    }
-
-    new SettingEx(this.containerEl)
-      .setName('Should update title key')
-      .setDesc('Whether to update the title key in frontmatter.')
-      .addToggle((toggle) => {
-        this.bind(toggle, 'shouldUpdateTitleKey');
+    new SettingGroupEx(this.containerEl)
+      .setHeading('Title')
+      .addSettingEx((setting) => {
+        setting
+          .setName('Should update title key')
+          .setDesc('Whether to update the title key in frontmatter.')
+          .addToggle((toggle) => {
+            this.bind(toggle, 'shouldUpdateTitleKey');
+          });
+      })
+      .addSettingEx((setting) => {
+        setting
+          .setName('Should store invalid title')
+          .setDesc(createFragment((f) => {
+            f.appendText('Whether to store the title with invalid characters.');
+            f.createEl('br');
+            f.appendText('If disabled, stores the sanitized version.');
+          }))
+          .addToggle((toggle) => {
+            this.bind(toggle, 'shouldStoreInvalidTitle');
+          });
       });
 
-    new SettingEx(this.containerEl)
-      .setName('Should update first header')
-      .setDesc(createFragment((f) => {
-        f.appendText('Whether to update the first header if it is present in the note. May conflict with the ');
-        f.createEl('a', {
-          attr: {
-            href: 'https://obsidian.md/plugins?id=obsidian-filename-heading-sync'
-          },
-          text: 'Filename Heading Sync'
-        });
-        f.appendText(' plugin.');
-      }))
-      .addToggle((toggle) => {
-        this.bind(toggle, 'shouldUpdateFirstHeader');
-      });
-
-    new SettingEx(this.containerEl)
-      .setName('Should support non-markdown files')
-      .setDesc(createFragment((f) => {
-        f.appendText('Whether to support non-markdown files.');
-        f.createEl('br');
-        f.appendText('If disabled, context menu and ');
-        appendCodeBlock(f, 'Smart rename');
-        f.appendText(' command will not be available for non-markdown files.');
-      }))
-      .addToggle((toggle) => {
-        this.bind(toggle, 'shouldSupportNonMarkdownFiles');
+    new SettingGroupEx(this.containerEl)
+      .setHeading('Other')
+      .addSettingEx((setting) => {
+        setting
+          .setName('Should update first header')
+          .setDesc(createFragment((f) => {
+            f.appendText('Whether to update the first header if it is present in the note. May conflict with the ');
+            f.createEl('a', {
+              attr: {
+                href: 'https://obsidian.md/plugins?id=obsidian-filename-heading-sync'
+              },
+              text: 'Filename Heading Sync'
+            });
+            f.appendText(' plugin.');
+          }))
+          .addToggle((toggle) => {
+            this.bind(toggle, 'shouldUpdateFirstHeader');
+          });
+      })
+      .addSettingEx((setting) => {
+        setting
+          .setName('Should support non-markdown files')
+          .setDesc(createFragment((f) => {
+            f.appendText('Whether to support non-markdown files.');
+            f.createEl('br');
+            f.appendText('If disabled, context menu and ');
+            appendCodeBlock(f, 'Smart rename');
+            f.appendText(' command will not be available for non-markdown files.');
+          }))
+          .addToggle((toggle) => {
+            this.bind(toggle, 'shouldSupportNonMarkdownFiles');
+          });
       });
   }
 }
